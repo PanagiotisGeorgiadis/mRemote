9520158c5a6a0aae72639924e1725e1234a2d3af
Panagiwtis Georgiadis <panagiwtis.geor@gmail.com>
2017-08-19 19:48:44 +0100

    Added the new actions from the Shared, Socket and InformationMessage action creators also refactored the code on the render function because it was redering the loadingImage along with the QRCodeImage component. Also added a noInternetConnection section but there is a page that needs to be created there.

--- public/containers/ConnectDevicePageContainer.jsx
+++ public/containers/ConnectDevicePageContainer.jsx
@@ -2,28 +2,88 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
-import { showLoadingImage, hideLoadingImage, toggleLoadingImage,
-		 getServerInfo, } from "../actions/ConnectDevicePageActions";
+import { showLoadingImage } from "../actions/SharedActions";
+import { initSocketConnection, updateHostInternetAccess, getConnectedDevices, updateConnectedDevices } from "../actions/SocketActions";
+import { showSuccessInformationMessage, showDangerInformationMessage, hideInformationMessage, updateInformationMessageText } from "../actions/InformationMessageActions";
+
+import { getServerInfo } from "../actions/ConnectDevicePageActions";
 
 import LoadingImage from "../components/Generic/LoadingImage";
+import InformationMessage from "../components/Generic/InformationMessage";
 import QRSectionContainer from "../components/ConnectDevicePage/QRSectionContainer";
 
 class ConnectDevicePageContainer extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			socketInitialised: false,
+		};
+	}
+
+	closeMessageHandler() {
+		this.state.hideInformationMessage();
+	}
+
+	closeMessageHandlerTimed() {
+		setTimeout(() => {
+			this.state.hideInformationMessage();
+		}, 5000);
+	}
+
+	initialiseSocketEvents(socket) {
+
+		socket.on("Host/Internet/Access", (internetAccess) => {
+			this.state.updateHostInternetAccess(internetAccess);
+		});
+
+		socket.on("Device/Connection/Success", () => {
+			// console.log("%c Device Connected!", "font-size: 16px; color: green;");
+			this.state.updateInformationMessageText("Device Connected!");
+			this.state.showSuccessInformationMessage();
+			this.state.getConnectedDevices();
+
+			this.closeMessageHandlerTimed();
+		});
+
+		socket.on("Device/Connection/Failure", () => {
+			// console.log("%c Device Disconnected!", "font-size: 16px; color: red;");
+			this.state.updateInformationMessageText("Device Disconnected!");
+			this.state.showDangerInformationMessage();
+			this.state.getConnectedDevices();
+
+			this.closeMessageHandlerTimed();
+		});
+
+		socket.on("Host/Connected_Devices", (connectedDevices) => {
+			this.state.updateConnectedDevices(connectedDevices);
+		});
+
+		this.setState({
+			socketInitialised: true
+		});
 	}
 
 	componentWillReceiveProps(nextProps) {
+		if(nextProps.socket && !this.state.socketInitialised) {
+			this.initialiseSocketEvents(nextProps.socket);
+		}
+
+		if(nextProps.internetAccess && !this.state.internetAccess) { // Refresh information after network disconnect.
+			nextProps.showLoadingImage();
+			nextProps.getServerInfo();
+		}
+
 		this.setState({
 			...nextProps
 		});
 	}
 
 	componentWillMount() {
+		this.props.initSocketConnection();
 		this.props.showLoadingImage();
 		this.props.getServerInfo();
+		this.props.getConnectedDevices();
 		this.setState({
 			...this.props
 		});
@@ -31,31 +91,49 @@ class ConnectDevicePageContainer extends Component {
 
 	render() {
 		let contents = [];
-		if(this.state.loadingImageVisible)
-			contents.push(<LoadingImage key = { 0 } />);
 
-        if(this.state.QRCodeImageSource)
-            contents.push(<QRSectionContainer key = { 1 } QRCodeImageSource = { this.state.QRCodeImageSource } toggleButtonText = { "Show Ip Address" } />);
+		contents.push(<InformationMessage key = {0} messageText = { this.state.informationMessageText } messageContainerClassName = { this.state.messageContainerClassName } windowContainerStyles = { this.state.windowContainerStyles } closeMessageHandler = { this.closeMessageHandler.bind(this) } />);
 
-		return(
+		if(this.state.loadingImageVisible) {
+			contents.push(<LoadingImage key = {1} />);
+		} else if (this.state.QRCodeImageSource) {
+			contents.push(<QRSectionContainer key = {1} QRCodeImageSource = { this.state.QRCodeImageSource } toggleButtonText = { "Show Ip Address" } />);
+		}
+
+        if(!this.state.internetAccess && this.state.socketInitialised) {
+        	// Create the No internet access page.
+			contents = <div>{"No Internet Access"}</div>;
+        }
+
+		return (
 			<div className = { "content_container" } >
-			   { contents }
+				{ contents }
 			</div>
 		);
 	}
 }
 
-const mapStateToProps = ({ConnectDevicePageReducer}) => {
+const mapStateToProps = ({ConnectDevicePageReducer, SocketReducer, SharedReducer, InformationMessageReducer}) => {
 	return {
-		...ConnectDevicePageReducer
+		...ConnectDevicePageReducer,
+		...SocketReducer,
+		...SharedReducer,
+		...InformationMessageReducer,
 	}
 };
 
 const mapDispatchToProps = (dispatch) => {
 	return bindActionCreators({
 		showLoadingImage,
-		hideLoadingImage,
-		getServerInfo
+		getServerInfo,
+		initSocketConnection,
+		updateHostInternetAccess,
+		showSuccessInformationMessage,
+		showDangerInformationMessage,
+		hideInformationMessage,
+		updateInformationMessageText,
+		getConnectedDevices,
+		updateConnectedDevices
 	}, dispatch);
 };
 
