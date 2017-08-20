d0d9e4f4952f77c7b27a402db3d616398b956c4b
Panagiwtis Georgiadis <panagiwtis.geor@gmail.com>
2017-08-20 13:14:20 +0100

    Added all the new reducers in the combine reducers function

--- public/reducers/index.js
+++ public/reducers/index.js
@@ -1,11 +1,19 @@
 import { combineReducers } from "redux";
 
+import SocketReducer from "./SocketReducer";
+import SharedReducer from "./SharedReducer";
 import MainMenuReducer from "./MainMenuReducer";
 import ConnectDevicePageReducer from "./ConnectDevicePageReducer";
+import InformationMessageReducer from "./InformationMessageReducer";
+import ConnectedDevicesPageReducer from "./ConnectedDevicesPageReducer";
 
 const rootReducer = combineReducers({
+	SocketReducer,
+	SharedReducer,
 	MainMenuReducer,
 	ConnectDevicePageReducer,
+	InformationMessageReducer,
+	ConnectedDevicesPageReducer,
 });
 
 
